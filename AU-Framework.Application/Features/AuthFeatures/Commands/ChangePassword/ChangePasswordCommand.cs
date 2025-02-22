6de68a357a63a18e0641244dbda8e35a42d7e3d3
Arif Uçar <102770456+LHOST1@users.noreply.github.com>
2025-02-23 00:22:56 +0300

    şifre değiştime işlemleri düzenlendi

--- AU-Framework.Application/Features/AuthFeatures/Commands/ChangePassword/ChangePasswordCommand.cs
+++ AU-Framework.Application/Features/AuthFeatures/Commands/ChangePassword/ChangePasswordCommand.cs
@@ -4,7 +4,6 @@ using MediatR;
 namespace AU_Framework.Application.Features.AuthFeatures.Commands.ChangePassword;
 
 public sealed record ChangePasswordCommand(
-    Guid UserId,
     string CurrentPassword,
     string NewPassword
 ) : IRequest<MessageResponse>; 
--- AU-Framework.Application/Features/AuthFeatures/Commands/ChangePassword/ChangePasswordCommandHandler.cs
+++ AU-Framework.Application/Features/AuthFeatures/Commands/ChangePassword/ChangePasswordCommandHandler.cs
@@ -7,16 +7,23 @@ namespace AU_Framework.Application.Features.AuthFeatures.Commands.ChangePassword
 public sealed class ChangePasswordCommandHandler : IRequestHandler<ChangePasswordCommand, MessageResponse>
 {
     private readonly IAuthService _authService;
+    private readonly ICurrentUser _currentUser;
 
-    public ChangePasswordCommandHandler(IAuthService authService)
+    public ChangePasswordCommandHandler(
+        IAuthService authService,
+        ICurrentUser currentUser)
     {
         _authService = authService;
+        _currentUser = currentUser;
     }
 
     public async Task<MessageResponse> Handle(ChangePasswordCommand request, CancellationToken cancellationToken)
     {
+        if (!_currentUser.IsAuthenticated)
+            throw new Exception("Kullanıcı oturum açmamış!");
+
         await _authService.ChangePasswordAsync(
-            request.UserId,
+            _currentUser.UserId,
             request.CurrentPassword,
             request.NewPassword,
             cancellationToken);
